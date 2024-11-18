6c064f8a918af5b90f8006af4fc8cf93c2236bfa
André da Silva <andre-da.silva@outlook.com>
2024-11-18 12:37:06 +0100

    Add 2 more problems. Also add formatting and update CI file to not build

--- lib/ocaml_99_problems.ml
+++ lib/ocaml_99_problems.ml
@@ -1,5 +1,19 @@
-let rec last (list: string list) =
+let rec last (list : string list) =
   match list with
   | [] -> None
-  | [res] -> Some(res)
+  | [ res ] -> Some res
   | _ :: tail -> last tail
+;;
+
+let rec last_two (list : string list) =
+  match list with
+  | [] | [ _ ] -> None
+  | [ a; b ] -> Some (a, b)
+  | _ :: tail -> last_two tail
+;;
+
+let rec nth_element_of_list (list : string list) (index : int) =
+  match list with
+  | [] -> None
+  | head :: tail -> if index > 0 then nth_element_of_list tail (index - 1) else Some head
+;;
--- test/ocaml_99_problems.ml
+++ test/ocaml_99_problems.ml
@@ -1,13 +1,39 @@
 open Alcotest
 
-let tail_of_a_list () =
+let test_tail_of_a_list () =
   let test_func = Ocaml_99_problems.last in
+  check (option string) "last is some d" (Some "d") (test_func [ "a"; "b"; "c"; "d" ]);
+  check (option string) "none" None (test_func [])
+;;
 
-  check (option string) "last is some d" (Some "d") (test_func ["a" ; "b" ; "c" ; "d"]);
-  check (option string) "none" (Some("a")) (test_func [])
+let test_last_two_elements_of_a_list () =
+  let test_func = Ocaml_99_problems.last_two in
+  check
+    (option (pair string string))
+    "last is some d"
+    (Some ("c", "d"))
+    (test_func [ "a"; "b"; "c"; "d" ]);
+  check (option (pair string string)) "none" None (test_func [ "a" ]);
+  check (option (pair string string)) "none" None (test_func [])
+;;
 
+let test_nth_element_of_list () =
+  let test_func = Ocaml_99_problems.nth_element_of_list in
+  check (option string) "none" (Some "c") (test_func [ "a"; "b"; "c"; "d"; "e" ] 2);
+  check (option string) "none" None (test_func [ "a" ] 2)
+;;
 
 let () =
-  Alcotest.run "OCaml 99 Problems" [
-    "1.", [Alcotest.test_case "Tail of a List" `Quick tail_of_a_list]
-  ]
+  Alcotest.run
+    "OCaml 99 Problems"
+    [ "1.", [ Alcotest.test_case "Tail of a List (Beginner)" `Quick test_tail_of_a_list ]
+    ; ( "2."
+      , [ Alcotest.test_case
+            "Last Two Elements of a List (Beginner)"
+            `Quick
+            test_last_two_elements_of_a_list
+        ] )
+    ; ( "3."
+      , [ Alcotest.test_case "N'th Element of a List (Beginner)" `Quick test_nth_element_of_list ] )
+    ]
+;;
